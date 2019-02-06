4dc84316a6905f029f41dceb371806b48a303a97
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-02-06 01:03:35 -0700

    Update `USER_WORK_EXPERIENCE` in Script.sql
    1. Add a column for `LOCATION`

--- Script.sql
+++ Script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 06, 2019 at 07:25 AM
+-- Generation Time: Feb 06, 2019 at 08:01 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -92,6 +92,7 @@ CREATE TABLE `USER_WORK_EXPERIENCE` (
   `ID` int(11) NOT NULL,
   `POSITION` varchar(45) DEFAULT NULL,
   `COMPANY` varchar(45) DEFAULT NULL,
+  `LOCATION` text NOT NULL,
   `START_YEAR` varchar(45) DEFAULT NULL,
   `END_YEAR` varchar(45) DEFAULT NULL,
   `ADDITIONAL_INFORMATION` varchar(45) DEFAULT NULL,
