a6759db4d34e39c79a3afce9c16d20b5f25deebf
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-02-06 00:27:19 -0700

    Update `USERS` in Script.sql
    1. Change the data type of `ROLE` to an int
    2. Created a new column for `SUSPEND` with a data type of a boolean to allow the suspension of users

--- Script.sql
+++ Script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 05, 2019 at 05:39 PM
+-- Generation Time: Feb 06, 2019 at 07:25 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -55,15 +55,17 @@ CREATE TABLE `USERS` (
   `EMAIL` varchar(45) DEFAULT NULL,
   `USERNAME` varchar(45) DEFAULT NULL,
   `PASSWORD` varchar(45) DEFAULT NULL,
-  `ROLE` varchar(45) DEFAULT NULL
+  `ROLE` int(11) DEFAULT NULL,
+  `SUSPEND` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `USERS`
 --
 
-INSERT INTO `USERS` (`ID`, `FIRSTNAME`, `LASTNAME`, `EMAIL`, `USERNAME`, `PASSWORD`, `ROLE`) VALUES
-(1, 'Mariah', 'Valenzuela', 'Mariahv08@gmail.com', 'Mariah', '1', '1'),
+INSERT INTO `USERS` (`ID`, `FIRSTNAME`, `LASTNAME`, `EMAIL`, `USERNAME`, `PASSWORD`, `ROLE`, `SUSPEND`) VALUES
+(1, 'Mariah', 'Valenzuela', 'Mariahv08@gmail.com', 'Mariah', '1', 1, 0);
+
 -- --------------------------------------------------------
 
 --
