bbaed3a42bbc0862e79e258130d53dad5565483f
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-02-10 01:48:05 -0700

    Update database.php
    Update 'mysql'

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'localhost'),//updated
+            'port' => env('DB_PORT', '8889'),//updated
+            'database' => env('DB_DATABASE', 'NetworkingWebsite'),//updated
+            'username' => env('DB_USERNAME', 'root'),//updated
+            'password' => env('DB_PASSWORD', 'root'),//updated
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
