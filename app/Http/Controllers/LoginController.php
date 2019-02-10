0cfb465208a8fd63df60882c16c4f53ce5545105
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-02-10 01:57:12 -0700

    Add files via upload

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,33 +8,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\BusinessServices\UserBusinessService;
 use Illuminate\Http\Request;
-use App\BusinessServices\UserBusinessService;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use PHPUnit\Exception;
+
+session_start();
 
 class LoginController extends Controller
-{
+{  
     public function index(Request $request){
         
-        //Store the form data
-        $username = $request->input('username');
-        $password = $request->input('pass');
-       
-        //Create a new business service
-        $bs = new UserBusinessService();
-       
-        //Use the business service object to attempt to login a user
-        //If the user information is valid the user will see that they are logged in
-        //If not ther will be prompted to try again
-        if($bs->login($username, $password)){
+        try{
+            //validate the form data
+            $this->validateForm($request);
             
-            //Render a response View with success message
-            return view('successfulView');
-                    
-        }else{
-            
-            //Render a response View with unsuccessful message
-            return view('unsuccessfulView');
-          
-        }      
+            //Store the form data
+            $username = $request->input('username');
+            $password = $request->input('pass');
+           
+            //Create a new business service
+            $bs = new UserBusinessService();
+           
+            //Use the business service object to attempt to login a user
+                //If the user information is valid the user will see that they are logged in
+                //If not they will be prompted to try again
+            $userId = $bs->login($username, $password);
+       
+            if($userId != null){ 
+                
+                //set session user id
+                $request->session()->put('userid', $userId);
+                
+                //use user id to find the user role and set a role session variable
+                $user = $bs->findById($userId);
+                $role = $user->getRole();
+                $request->session()->put('role', $role);
+                
+                //Render a response View with success message
+                return view('userProfileView');
+                        
+            }else{
+                
+                //Render a response View with unsuccessful message
+                return view('unsuccessfulView');             
+            }   
+        }
+        catch(ValidationException $e1) {
+            throw $e1;
+        }
+        catch (Exception $e){       
+            Log::error("Exception ", array("message" => $e->getMessage()));
+            $data = ['errorMsg' => $e->getMessage()];
+            return view('exception')->with($data);
+        }
+    }
+    
+    private function validateForm(Request $request) {
+        //setup data validattion rules
+        $rules = ['username' => 'Required | Between: 4,20| Alpha','pass' => 'Required | Between: 1,20'];
+        
+        //run data validation rules
+        $this->validate($request, $rules);
     }
 }
--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -8,43 +8,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
+use App\Services\BusinessServices\UserBusinessService;
 use Illuminate\Http\Request;
-use App\BusinessServices\UserBusinessService;
-use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use PHPUnit\Exception;
 
 
 class RegistrationController extends Controller
 {
     public function index(Request $request){
         
-        //Store the form data
-        $first = $request->input('firstname');
-        $last = $request->input('lastname');
-        $email = $request->input('email');
-        $username = $request->input('username');
-        $password = $request->input('pass');
-        
-        
-        //Create a new business service
-        $bs = new UserBusinessService();
-        
-        //Create a new user object with the form data
-        $newUser = new User(0, $first, $last, $email, $username, $password, 0);
-        
-        //Use the business service object to create a new user in the database
-        //$bs->createNewUser($newUser);
-        
-        if($bs->createNewUser($newUser)){
-            //Render a response View with success message
-            return view('successfulView');
+        try{
+            //validate the form data
+            $this->validateForm($request);
             
-        }else{
+            //Store the form data
+            $first = $request->input('firstname');
+            $last = $request->input('lastname');
+            $email = $request->input('email');
+            $username = $request->input('username');
+            $password = $request->input('pass');
+                      
+            //Create a new business service
+            $bs = new UserBusinessService();
             
-            //Render a response View with unsuccessful message
-            return view('unsuccessfulView');
+            //Create a new user object with the form data
+            $newUser = new UserModel(0, $first, $last, $email, $username, $password, 0);
             
+            //Use the business service object to create a new user in the database
+            if($bs->createNewUser($newUser)){
+                //Render a response View with success message
+                return view('loginView');
+                
+            }else{                
+                //Render a response View with unsuccessful message
+                return view('unsuccessfulView');               
+            }
+        }
+        catch(ValidationException $e1) {
+            throw $e1;
+        }
+        catch (Exception $e){
+            Log::error("Exception ", array("message" => $e->getMessage()));
+            $data = ['errorMsg' => $e->getMessage()];
+            return view('exception')->with($data);
         }
                 
+    }
+    private function validateForm(Request $request) {
+        //setup data validattion rules
+        $rules = ['firstname' => 'Required | Between: 4,20| Alpha','lastname' => 'Required | Between: 4,20| Alpha', 'email' => 'Required | E-mail', 'username' => 'Required | Between: 4,20| Alpha','pass' => 'Required | Between: 1,20'];
+        
+        //run data validation rules
+        $this->validate($request, $rules);
     }
     
 }
