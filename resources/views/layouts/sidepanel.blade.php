fd68f05ef91f372f41c5cf2335cd7a6bee5ed061
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-03-03 19:29:07 -0700

    Add "Home Page" Link to Side Panel

--- resources/views/layouts/sidepanel.blade.php
+++ resources/views/layouts/sidepanel.blade.php
@@ -3,7 +3,7 @@
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                	<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                	<a class="nav-link" href="home">Home<span class="sr-only"></span></a>
                 </li>
                 <li class="nav-item dropdown">
                 	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
