bf040461c5e00b9831205967337e048e80386a63
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-03-04 05:20:00 -0700

    Update Files to Include Security
    Users must login to access the pages of the website other than home, login, and registration

--- resources/views/homeView.blade.php
+++ resources/views/homeView.blade.php
@@ -5,18 +5,31 @@
 //This is my own work.
 //This page displays the home page 
 -->
- 
-@extends('layouts.appmaster')
-@section('title', 'Home Page')
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+    	<title>Home Page</title>
+    	
+    	<!-- Font -->
+    	<link href="https://fonts.googleapis.com/css?family=Noto+Serif+SC" rel="stylesheet">
+        <!-- Side Panel -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">    
+        <!-- My Stylesheet -->
+        <link rel="stylesheet" type="text/css" href="/NetworkingMilestone/public/css/styles.css" /> 
+            
+</head> 
   
-@section('content')
+<body>
+@include('layouts.sidepanel')
+
 <div class = home>
 
 <h1>Welcome to Networking.com</h1>
 <p>This is some networking site that I will name and fix the CSS on soon.</p>
 
 </div>
-<!-- check if the session variable holds the user id -->
-@if (session()->has('userid'))
-@endif 
-@endsection
+</body>
+</html>
--- resources/views/registrationView.blade.php
+++ resources/views/registrationView.blade.php
@@ -5,11 +5,26 @@
 //This is my own work.
 //The registration form
  -->
- 
-@extends('layouts.appmaster')
-@section('title', 'Registration Page')
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+    	<title>Registration Page</title>
+    	
+    	<!-- Font -->
+    	<link href="https://fonts.googleapis.com/css?family=Noto+Serif+SC" rel="stylesheet">
+        <!-- Side Panel -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">    
+        <!-- My Stylesheet -->
+        <link rel="stylesheet" type="text/css" href="/NetworkingMilestone/public/css/styles.css" /> 
+            
+</head> 
   
-@section('content')
+<body>
+@include('layouts.sidepanel')
+
 <div class = form>
     <form action = "registrationView" method = "POST">
     	<input type = "hidden" name = "_token" value = "<?php  echo csrf_token()?>">
@@ -46,4 +61,5 @@
 			{{$message}} <br/> 
 		@endforeach 
 	@endif
-@endsection
+</body>
+</html>
