be0d4d65f33fbeca4395325d72a832c52a0e2615
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-03-13 00:44:21 -0700

    Add link to an individual job page
    *uses the help of a form and hidden input type for the id

---
+++ resources/views/jobsView.blade.php
@@ -0,0 +1,58 @@
+<!--
+//Almicke Navarro
+//3-12-19
+//Networking Milestone
+//This is my own work.
+//The form that allows users to view, save, and apply to all jobs
+-->
+
+@extends('layouts.appmaster')
+@section('title', 'Job(s) Page')
+  
+@section('content')
+
+@if(session()->has('role'))
+    <h3 class = "heading1">Find New Jobs</h3>
+    <hr> 
+    @if ($jobs != null)
+    <table id = jobs class = "darkTable table-hover"> 
+    <thead>	
+        <th>Position</th>
+        <th>Company</th>
+    	<th>Pay</th>
+    	<th>Description</th>
+    	<th>Address</th>
+    	<th></th>
+    	<th></th>
+    </thead>
+    <tbody>
+
+    <!-- Loop to show all the jobs from the database -->
+    @for ($x = 0; $x < count($jobs); $x++) 
+        <tr>   
+        <td><form action = 'jobView' method = 'GET'><input type = 'hidden' name = 'id' value = {{$jobs[$x]['ID']}}><input type = 'submit' value = '{{$jobs[$x]['NAME']}}'></form> </td>
+        <td> {{$jobs[$x]['COMPANY']}} </td>
+        <td> {{$jobs[$x]['PAY']}} </td>
+        <td> {{$jobs[$x]['DESCRIPTION']}} </td>
+        <td> INSERT ADDRESS </td>
+        <td><form action = 'saveJobView' method = 'GET'><input type = 'hidden' name = 'id' value = {{$jobs[$x]['ID']}}><input type = 'submit' value = 'Save'></form> </td>
+        <td><form action = 'applyJobView' method = 'GET'><input type = 'hidden' name = 'id' value = {{$jobs[$x]['ID']}}><input type = 'submit' value = 'Apply'></form> </td>
+        
+        </tr>
+   
+    @endfor
+    </tbody>
+	</table>
+	@endif
+@else 
+<h4>Sorry, you must login to view this page!</h4>
+@endif 
+
+<script type="text/javascript">
+$(document).ready( function () {
+    $('#jobs').DataTable();
+} );
+</script>
+            	
+
+@endsection
