28932a89ae6e5952203fd0dd6e923bed0afc6984
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-03-20 04:34:21 -0700

    Create readSaved() & readApplied()
    *these methods will get the jobs that the user saved and applied to from the database

--- app/Services/DataServices/UsersJobPostingsDataService.php
+++ app/Services/DataServices/UsersJobPostingsDataService.php
@@ -52,12 +52,12 @@ class UsersJobPostingsDataService{
     }
     
     //accepts an user id; finds the user job postings in the database with a matching id
-    function readByUserID($id){
+    function readSaved($id){
         try {
-            Log::info("Entering UsersJobPostingsDataService.readByUserID()");
+            Log::info("Entering UsersJobPostingsDataService.readSaved()");
             
             //use the connection to create a prepared statement
-            $stmt = $this->conn->prepare("SELECT * FROM `USERS_JOB_POSTINGS` WHERE USERS_ID = :id");
+            $stmt = $this->conn->prepare("SELECT `JOB_POSTINGS`.`ID`, `JOB_POSTINGS`.`NAME`, `JOB_POSTINGS`.`COMPANY`, `JOB_POSTINGS`.`PAY`, `JOB_POSTINGS`.`DESCRIPTION` FROM `JOB_POSTINGS` JOIN `USERS_JOB_POSTINGS` ON `JOB_POSTINGS`.`ID` = `USERS_JOB_POSTINGS`.`JOB_POSTINGS_ID` WHERE `USERS_ID` = :id AND `SAVE` = 1");
             
             //Bind the variables from the user object to the SQL statement
             $stmt->bindParam(':id', $id);
@@ -67,7 +67,7 @@ class UsersJobPostingsDataService{
             
             //check is a row was returned
             if($stmt->rowCount() == 0){
-                Log::info("Exiting UsersJobPostingsDataService.readByUserID() with returning null");
+                Log::info("Exiting UsersJobPostingsDataService.readSaved() with returning null");
                 return null;
             }
             else{
@@ -81,8 +81,48 @@ class UsersJobPostingsDataService{
                     
                 }
                               
-                Log::info("Exiting UsersJobPostingsDataService.readByUserID() with returning a skill object");
+                Log::info("Exiting UsersJobPostingsDataService.readSaved() with returning a saved jobs array");
                 return $uj_arr;            
+            }
+        }
+        catch (PDOException $e){
+            Log::error("Exception: ", array("message" => $e->getMessage()));
+            throw new DatabaseException("Database Exception: " . $e->getMessage(), 0, $e);
+        }
+    }
+    
+    //accepts an user id; finds the user job postings in the database with a matching id
+    function readApplied($id){
+        try {
+            Log::info("Entering UsersJobPostingsDataService.readApplied()");
+            
+            //use the connection to create a prepared statement
+            $stmt = $this->conn->prepare("SELECT `JOB_POSTINGS`.`ID`, `JOB_POSTINGS`.`NAME`, `JOB_POSTINGS`.`COMPANY`, `JOB_POSTINGS`.`PAY`, `JOB_POSTINGS`.`DESCRIPTION` FROM `JOB_POSTINGS` JOIN `USERS_JOB_POSTINGS` ON `JOB_POSTINGS`.`ID` = `USERS_JOB_POSTINGS`.`JOB_POSTINGS_ID` WHERE USERS_ID = :id AND `APPLY` = 1");
+            
+            //Bind the variables from the user object to the SQL statement
+            $stmt->bindParam(':id', $id);
+            
+            //execute the SQL statement
+            $stmt->execute();
+            
+            //check is a row was returned
+            if($stmt->rowCount() == 0){
+                Log::info("Exiting UsersJobPostingsDataService.readApplied() with returning null");
+                return null;
+            }
+            else{
+                //create an user groups array
+                $uj_arr = array();
+                
+                //loop to get all the users groups data to put into the array
+                while ($uj = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    
+                    array_push($uj_arr, $uj);
+                    
+                }
+                
+                Log::info("Exiting UsersJobPostingsDataService.readApplied() with returning a applied to jobs array");
+                return $uj_arr;
             }
         }
         catch (PDOException $e){
@@ -97,7 +137,7 @@ class UsersJobPostingsDataService{
             Log::info("Entering UsersJobPostingsDataService.delete()");
             
             //use the connection to create a prepared statement
-            $stmt = $this->conn->prepare("DELETE FROM `USERS_JOB_POSTINGS` WHERE `USERS_ID` = :id");
+            $stmt = $this->conn->prepare("DELETE FROM `USERS_JOB_POSTINGS` WHERE `JOB_POSTINGS_ID` = :id AND `SAVE` = 1");
             
             //Bind the variables to the SQL statement
             $stmt->bindParam(':id', $id);
