6d1f3ea92f7ec2194db386a95e10153af49fc385
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-04-09 04:05:13 -0700

    Insert Middleware into Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,9 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            
+            //include my own middleware
+            \App\Http\Middleware\SecurityMiddleware::class, 
         ],
 
         'api' => [
