b060a67ca6b3023035be8efbcc91b65bf162dffd
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-04-09 03:13:45 -0700

    Update Layouts Folder

--- resources/views/layouts/appmaster.blade.php
+++ resources/views/layouts/appmaster.blade.php
@@ -11,7 +11,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">    
         <!-- My Stylesheet -->
-        <link rel="stylesheet" type="text/css" href="/public/css/styles.css" /> 
+        <link rel="stylesheet" type="text/css" href="/NetworkingMilestone/public/css/styles.css" /> 
         <!-- Data Tables -->
         <script type="text/javascript" src="{{ URL::asset('//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js') }}"></script>
         <link rel="stylesheet" href="{{ URL::asset('//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css') }}" />
